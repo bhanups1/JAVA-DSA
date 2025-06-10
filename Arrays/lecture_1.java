98ae09e7e1e4137b0311ae70dec8126ddfb643a1
Bhanups1 <bhanupartap947247@gamil.com>
2025-06-10 22:24:09 +0530

    find lenght

--- Arrays/lecture_1.java
+++ Arrays/lecture_1.java
@@ -13,26 +13,30 @@ public class lecture_1 {
         // int phy;
         // phy = sc.nextInt();
 
-        marks[0] = sc.nextInt();    // phy
-        marks[1] = sc.nextInt();   //  chem
-        marks[2] = sc.nextInt();  //   math
 
-        System.out.println("phy : " + marks[0]);
-        System.out.println("chem : " + marks[1]);
-        System.out.println("math : " + marks[2]);
+        /// find length
+        System.out.println("length of array = " + marks.length);
 
-        // update value
+        // marks[0] = sc.nextInt();    // phy
+        // marks[1] = sc.nextInt();   //  chem
+        // marks[2] = sc.nextInt();  //   math
 
-        //marks[2] = 100;
-        marks[2] = marks[2] + 1;
-        marks[1] = marks[1] + 1;
-        marks[0] = marks[0] + 1;
+        // System.out.println("phy : " + marks[0]);
+        // System.out.println("chem : " + marks[1]);
+        // System.out.println("math : " + marks[2]);
 
-        // find percentage
+        // // update value
 
-        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+        // //marks[2] = 100;
+        // marks[2] = marks[2] + 1;
+        // marks[1] = marks[1] + 1;
+        // marks[0] = marks[0] + 1;
 
-        System.out.println("percentage =  " + percentage + " % ");
+        // // find percentage
+
+        // int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+
+        // System.out.println("percentage =  " + percentage + " % ");
     }
 
 }
