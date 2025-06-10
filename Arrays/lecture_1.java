9997febfa3f6ef4a321780fae09ae0411cf1848f
Bhanups1 <bhanupartap947247@gamil.com>
2025-06-10 22:18:52 +0530

    percentage finde

--- Arrays/lecture_1.java
+++ Arrays/lecture_1.java
@@ -13,9 +13,9 @@ public class lecture_1 {
         // int phy;
         // phy = sc.nextInt();
 
-        marks[0] = sc.nextInt();  // phy
-        marks[1] = sc.nextInt();   //chem
-        marks[2] = sc.nextInt();  // math
+        marks[0] = sc.nextInt();    // phy
+        marks[1] = sc.nextInt();   //  chem
+        marks[2] = sc.nextInt();  //   math
 
         System.out.println("phy : " + marks[0]);
         System.out.println("chem : " + marks[1]);
@@ -27,7 +27,12 @@ public class lecture_1 {
         marks[2] = marks[2] + 1;
         marks[1] = marks[1] + 1;
         marks[0] = marks[0] + 1;
-        System.out.println("math : " + marks[2]);
+
+        // find percentage
+
+        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+
+        System.out.println("percentage =  " + percentage + " % ");
     }
 
 }
